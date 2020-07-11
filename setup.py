1fb4b73801a1b6a25cb6fd6ccaf4005d299c21eb
Instrumentality Foundation <instrumentality-foundation@outlook.com>
2020-07-11 23:39:35 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='instrumentality-blockchain-api',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GPL-3.0-or-later',
+    author='Instrumentality Foundation',
+    author_email='instrumentality-foundation@outlook.com',
+    description=''
+)
